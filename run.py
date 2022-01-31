f7eee33ab82960a133d4094b79c184126eab53c4
uchanLee <yj2221@naver.com>
2022-02-01 00:11:15 +0900

    merge

--- run.py
+++ run.py
@@ -1,14 +1,13 @@
 from tactics.v1 import func_version1
-from utils import get_high_volume_tickers
-import pyupbit
 
-path = 'keys.json'
+path = "keys.json"
+
 
 def main():
-    EXCEPT_COINS = ['KRW-FLOW', 'KRW-ETH', 'KRW-ADA', 'KRW-MANA']
-    
-    
+    EXCEPT_COINS = ["KRW-FLOW", "KRW-ETH", "KRW-ADA", "KRW-MANA"]
+
     func_version1(EXCEPT_COINS)
 
-if __name__=='__main__':
+
+if __name__ == "__main__":
     main()
