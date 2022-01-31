00addc7cc2823d1db4e906217cf2daf561911107
uchanLee <yj2221@naver.com>
2022-01-31 19:31:23 +0900

    delete my API for replacing with pyupbit

--- utils.py
+++ utils.py
@@ -11,10 +11,3 @@ def read_key(path, key="ACCESS_KEY"):
             raise KeyError(e)
         else:
             return key
-
-
-def candidate_coins(upbit, selected_coins=None):
-    if selected_coins is not None:
-        return map(lambda x: "KRW-{0}".format(x), selected_coins)
-    candidate_coin = map(lambda x: x["market"], upbit.get_markets())
-    return filter(lambda x: x.startswith("KRW"), candidate_coin)
