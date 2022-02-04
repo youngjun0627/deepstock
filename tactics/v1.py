ab40ceaa462283fe2490eb877165bfa2bf0d8258
Ubuntu <ubuntu@ip-172-31-47-134.ap-northeast-2.compute.internal>
2022-02-04 13:38:53 +0000

    modify tactics for fixing bug

--- tactics/v1.py
+++ tactics/v1.py
@@ -5,6 +5,7 @@ import pyupbit
 from pytz import timezone
 
 from utils import get_high_volume_tickers
+from utils import get_kvalue
 from utils import read_key
 
 
@@ -43,61 +44,65 @@ def get_current_price(ticker):
 
 def func_version1(EXCEPT_COINS, slackbot, path="keys.json"):
 
-    buy_dict = {}
+    except_coins = []
+    except_coins += EXCEPT_COINS
+    buy_prices = {}
     markets = pyupbit.get_tickers(fiat="KRW")
-    markets = get_high_volume_tickers(markets, count=20)
-
+    markets = get_high_volume_tickers(markets, count=5)
     access = read_key(path)
     secret = read_key(path, key="SECRET_KEY")
     upbit = pyupbit.Upbit(access, secret)
     for market, _ in markets:
-        if market in EXCEPT_COINS:
+        if market in except_coins:
             continue
         balance = get_balance(upbit, market.split("-")[1])
         if balance != 0:
             avg_buy_price = upbit.get_avg_buy_price(market.split("-")[1])
-            buy_dict[market] = avg_buy_price
-    markets_count = len(markets)
+            buy_prices[market] = avg_buy_price
+    start_time = datetime.datetime.now().astimezone(timezone("Asia/Seoul")).replace(tzinfo=None)
+
     while True:
         for market, _ in markets:
-            if market in EXCEPT_COINS:
+            if market in except_coins:
                 continue
+            time.sleep(1)
             try:
                 now = datetime.datetime.now().astimezone(timezone("Asia/Seoul")).replace(tzinfo=None)
-                start_time = get_start_time(market)
-                end_time = start_time + datetime.timedelta(days=1)
-                if start_time < now < end_time - datetime.timedelta(seconds=markets_count + 1):
-                    if market in buy_dict:
-                        continue
-                    target_price = get_target_price(market, 0.5)
-                    ma15 = get_ma15(market)
+                if now < start_time + datetime.timedelta(seconds=60 * 60):
                     current_price = get_current_price(market)
+                    if market in buy_prices:
+                        if current_price < (buy_prices[market] * 0.95) or current_price > (buy_prices[market] * 1.05):
+                            crypto = get_balance(upbit, market.split("-")[1])
+                            result = upbit.sell_market_order(market, crypto)
+                            if result is not None:
+                                buy_prices.pop(market)
+                                except_coins += [market]
+                                slackbot.post_message(f"sell(d): {market} -> {crypto} won")
+                        continue
+                    k = get_kvalue(market)
+                    target_price = get_target_price(market, k)
+                    ma15 = get_ma15(market)
                     if target_price < current_price and ma15 < current_price:
                         krw = min(10000, get_balance(upbit, "KRW"))
                         if krw > 5000:
                             result = upbit.buy_market_order(market, krw * 0.9995)
                             if result is not None:
-                                buy_dict[market] = krw * 0.9995
+                                buy_prices[market] = current_price
                                 slackbot.post_message(f"buy: {market} -> {krw * 0.9995} won")
-                    if market in buy_dict:
-                        crypto = get_balance(upbit, market.split("-")[1])
-                        if (crypto * current_price) < (buy_dict[market] * 0.95):
-                            result = upbit.sell_market_order(market, crypto)
-                            if result is not None:
-                                buy_dict.pop(market)
-                                slackbot.post_message(f"sell(d): {market} -> {crypto} won")
 
                 else:
-                    crypto = get_balance(upbit, market.split("-")[1])
-                    if (crypto * get_current_price(market)) > 5000:
-                        result = upbit.sell_market_order(market, crypto)
-                        if result is not None:
-                            buy_dict.pop(market)
-                            slackbot.post_message(f"sell: {market} -> {crypto} won")
+                    buy_markets = list(buy_prices.keys())
+                    for market in buy_markets:
+                        crypto = get_balance(upbit, market.split("-")[1])
+                        if (crypto * get_current_price(market)) > 5000:
+                            result = upbit.sell_market_order(market, crypto)
+                            if result is not None:
+                                buy_prices.pop(market)
+                                slackbot.post_message(f"sell: {market} -> {crypto} won")
                     markets = pyupbit.get_tickers(fiat="KRW")
-                    markets = get_high_volume_tickers(markets)
-                time.sleep(1)
+                    markets = get_high_volume_tickers(markets, count=5)
+                    start_time = now
+                    except_coins = EXCEPT_COINS
             except Exception as e:
                 slackbot.post_message(e)
-                print(e)
                 time.sleep(1)
--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 import json
-import pyupbit
 import time
+
+import pyupbit
+
 
 def read_key(path, key="ACCESS_KEY"):
     with open(path, "r") as f:
@@ -14,12 +16,21 @@ def read_key(path, key="ACCESS_KEY"):
             return key
 
 
-def get_high_volume_tickers(coins, count=10):
+def get_high_volume_tickers(coins, count=10, interval="minute60"):
     result = []
     for coin in coins:
-        volumes = (coin, pyupbit.get_ohlcv(ticker=coin, count=1, interval='minute1')['value'].values[0])
+        volumes = (coin, pyupbit.get_ohlcv(ticker=coin, count=1, interval=interval)["value"].values[0])
         result.append(volumes)
         time.sleep(0.1)
-
-    result.sort(key=lambda x:x[1])
+    result.sort(key=lambda x: x[1])
     return result[-count:]
+
+
+def get_kvalue(market):  # 인자로 받은 종목에 대한 20일 average noise ratio.
+    try:
+        ohlck = pyupbit.get_ohlcv(ticker=market, count=20, interval="day")
+        ohlck["noiseratio"] = 1 - (abs(ohlck["open"] - ohlck["close"]) / (ohlck["high"] - ohlck["low"]))
+        return round(ohlck["noiseratio"].mean(), 2)
+    except Exception as ex:
+        print("get_kvalue() -> 에러: " + str(ex))
+        return None
