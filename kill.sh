2d4354a960137452e29ff7252861d41a008de95a
Ubuntu <ubuntu@ip-172-31-47-134.ap-northeast-2.compute.internal>
2022-02-05 08:59:53 +0000

    kill.sh 추가

---
+++ kill.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+
+# ps -ef을 이용해서 원하는 프로세스 정보를 얻는다.
+process=$(ps -ef | grep 'python run.py$')
+
+
+pid=$(echo ${process} | cut -d " " -f2)
+
+
+if [ -n "${pid}" ]
+then
+        result=$(kill -9 ${pid})
+        echo process is killed.
+else
+        echo running process not found.
+fi
+
