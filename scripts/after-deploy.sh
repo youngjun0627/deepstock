d93441dd46ba817380c787a2f30a711c877cca28
uchanLee <yj2221@naver.com>
2022-04-03 01:43:02 +0900

    s3 올라가는 지 검증

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -1,3 +1,21 @@
-REPOSITORY=/home/ubuntu/deep-stock
+#!/bin/sh
 
-ls
+
+# ps -ef을 이용해서 원하는 프로세스 정보를 얻는다.
+process=$(ps -ef | grep 'python3 main.py$')
+
+
+pid=$(echo ${process} | cut -d " " -f2)
+
+
+if [ -n "${pid}" ]
+then
+        result=$(kill -9 ${pid})
+        echo process is killed.
+else
+        echo running process not found.
+fi
+
+cd deepstock
+
+python3 main.py
